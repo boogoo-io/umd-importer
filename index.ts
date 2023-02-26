58207d7d08b5c8396ea63013ab11bfa2933e9841
hq5544 <baibai@outlook.com>
2023-02-26 22:27:49 +0800

    feat: version 1.0.0 done

---
+++ index.ts
@@ -0,0 +1,10 @@
+import UmdImporter from "./src/main"
+
+const umdImporter = new UmdImporter({debug: true})
+
+const [React, ReactDOM] = await Promise.all([
+  umdImporter.import<any>('https://unpkg.com/react@18/umd/react.development.js'),
+  umdImporter.import<any>('https://unpkg.com/react-dom@18/umd/react-dom.development.js'),
+])
+
+ReactDOM.createRoot(document.getElementById('app')).render(React.createElement('div', null, 'loaded'))
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// vite.config.js
+import {resolve} from 'path'
+import {defineConfig} from 'vite'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'UmdImporter',
+      fileName: 'main',
+      copyPublicDir: false,
+    },
+  },
+})
+
